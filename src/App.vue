70965deb9c9258d5cfe7cae0e7f5d1727a96800f
sampathdissanayaka <dmcsampath97@gmail.com>
2020-12-15 12:18:41 +0700

    add chartdata

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>Outdoor Temperature</h2>
+    <line-chart :data="chartData"></line-chart>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+       chartData: {
+     
+         "2018-12-01 10:20:12":"35C",
+         "2018-12-02 10:20:12":"3C",
+         "2018-12-03 10:20:12":"10C",
+         "2018-12-04 10:20:12":"22C",
+         "2018-12-05 10:20:12":"40C",
+         "2018-12-06 10:20:12":"16C",
+         "2018-12-07 10:20:12":"25C",
+        
+
+       }
     }
   }
 }
@@ -40,21 +37,5 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
